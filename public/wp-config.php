923dc9770611b2f601b077fe0ca8cae31c5fffe1
JL <67359294+johnliveeoroncillo@users.noreply.github.com>
2022-07-15 18:07:18 +0800

    spl

--- public/wp-config.php
+++ public/wp-config.php
@@ -134,3 +134,7 @@ if ( !defined("ABSPATH") )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . "wp-settings.php");
+
+spl_autoload_register(function($class) {
+    include 'classes/' . $class . '.class.php';
+});
